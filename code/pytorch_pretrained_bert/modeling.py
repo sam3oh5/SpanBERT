ca1fb5a27ba85c5295a196d07efa78babb057715
sam3oh5 <35@42maru.com>
2019-12-30 16:39:14 +0900

    [REFACTORING]

--- code/pytorch_pretrained_bert/modeling.py
+++ code/pytorch_pretrained_bert/modeling.py
@@ -1204,7 +1204,7 @@ class BertForQuestionAnswering(BertPreTrainedModel):
         padded_outputs = torch.cat((pad, sequence_output, pad), dim=1)
         catted_outputs = None
         for idx in range(1, length+1):
-            cat_op = torch.cat((padded_outputs[:,idx-1:idx,:], padded_outputs[:,idx:idx+1,:], padded_outputs[:,idx+1:idx+2,:]), dim=2)
+            cat_op = padded_outputs[:,idx-1:idx+2,:]
             if catted_outputs is None:
                 catted_outputs = cat_op
             else:
